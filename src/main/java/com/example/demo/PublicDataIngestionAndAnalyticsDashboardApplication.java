6004bec0b523fd5447fdaaca3f5c40babcab56a5
Devansh Pundir <devanshpundir7187@gmail.com>
2026-02-17 17:31:57 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/PublicDataIngestionAndAnalyticsDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicDataIngestionAndAnalyticsDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicDataIngestionAndAnalyticsDashboardApplication.class, args);
+	}
+
+}
