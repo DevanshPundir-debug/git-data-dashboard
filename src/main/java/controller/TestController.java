4ad46a0aca32ba3bd35c3330e8fdf334363b76bb
Devansh Pundir <devanshpundir7187@gmail.com>
2026-02-22 21:14:38 +0530

    try2

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -1,4 +1,5 @@
-package controller;
+//package controller;
+package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
